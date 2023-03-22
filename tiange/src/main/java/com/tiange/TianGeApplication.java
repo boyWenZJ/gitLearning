39ff869ee9c5eb494acac698d9681c3f339093f2
boyWenZJ <2296741980@qq.com>
2023-03-22 18:11:02 +0800

    tiange project init

---
+++ tiange/src/main/java/com/tiange/TianGeApplication.java
@@ -0,0 +1,13 @@
+package com.tiange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TianGeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TianGeApplication.class, args);
+    }
+
+}
---
+++ tiange/src/test/java/com/tiange/TianGeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TianGeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
