03599459b5f28b7371ca0f0366b026d569c8c9a4
boyWenZJ <2296741980@qq.com>
2023-03-23 17:20:44 +0800

    修改启动类名称

--- tiange/tiange-admin/src/test/java/com/tiange/admin/TianGeApplicationTests.java
+++ tiange/tiange-admin/src/test/java/com/tiange/admin/TianGeApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TiangeAdminApplicationTests {
+class TianGeApplicationTests {
 
     @Test
     void contextLoads() {
