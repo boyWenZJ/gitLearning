a7b209348c22a9ec5d19a84ff9921f13ef8a3dd6
boyWenZJ <2296741980@qq.com>
2023-03-23 14:34:59 +0800

    init

---
+++ tiange/tiange-admin/src/main/java/com/tiange/admin/TiangeAdminApplication.java
@@ -0,0 +1,13 @@
+package com.tiange.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiangeAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiangeAdminApplication.class, args);
+    }
+
+}
